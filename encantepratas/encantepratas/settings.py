4941d3154a859a656cc48c2c1db04758b008f6c4
briellydiassilva <146475496+briellydiassilva@users.noreply.github.com>
2026-08-04 13:11:54 -0300

    Criação dos apps.

--- encantepratas/encantepratas/settings.py
+++ encantepratas/encantepratas/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    #meus apps: 
+    'categorias',
+    'clientes',
+    'pagamentos',
+    'pedidos',
+    'produtos',
 ]
 
 MIDDLEWARE = [
