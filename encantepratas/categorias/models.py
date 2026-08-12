308ed522de9a666c3ca2eb14c3263b4b440e2090
lorranirodriguess <146475221+lorranirodriguess@users.noreply.github.com>
2026-08-12 14:41:21 -0300

    Criando os models de categoria e usuario

--- encantepratas/categorias/models.py
+++ encantepratas/categorias/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    ativo = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
+        ordering = ['nome']
+
+    def __str__(self):
+        return self.nome
+
--- encantepratas/usuarios/models.py
+++ encantepratas/usuarios/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Usuario(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='usuario')
+    cpf = models.CharField(max_length=11, unique=True)
+    telefone = models.CharField(max_length=20)
+    cep = models.CharField(max_length=9)
+    bairro = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=100)
+    estado = models.CharField(max_length=2)
+    rua = models.CharField(max_length=200)
+    numero_casa = models.CharField(max_length=10)
+
+    class Meta:
+        verbose_name = 'Usuário'
+        verbose_name_plural = 'Usuários'
+
+    def __str__(self):
+        return self.user.get_full_name() or self.user.username
