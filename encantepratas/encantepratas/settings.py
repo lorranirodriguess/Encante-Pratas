f40f7e889d67d70a6a0488d8b83cabe911decaff
briellydiassilva <146475496+briellydiassilva@users.noreply.github.com>
2026-08-12 13:49:41 -0300

    Adicionando os apps no settings

--- encantepratas/encantepratas/settings.py
+++ encantepratas/encantepratas/settings.py
@@ -38,6 +38,13 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    #MEUS APPS: 
+    'categorias',
+    'pedidos',
+    'produtos',
+    'usuarios',
+    'pagamentos',
+
 ]
 
 MIDDLEWARE = [
